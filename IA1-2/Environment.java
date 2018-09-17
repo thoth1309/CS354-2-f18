557f97c3f53500410dd786f33f102a91cbb3cd71
Jason Egbert <jasonegbert@u.boisestate.edu>
2018-09-17 13:00:02 -0600

    stub for IA1-2

---
+++ IA1-2/Environment.java
@@ -0,0 +1,21 @@
+// This class provides a stubbed-out environment.
+// You are expected to implement the methods.
+// Accessing an undefined variable should throw an exception.
+
+// Hint!
+// Use the Java API to implement your Environment.
+// Browse:
+//   https://docs.oracle.com/javase/tutorial/tutorialLearningPaths.html
+// Read about Collections.
+// Focus on the Map interface and HashMap implementation.
+// Also:
+//   https://www.tutorialspoint.com/java/java_map_interface.htm
+//   http://www.javatpoint.com/java-map
+// and elsewhere.
+
+public class Environment {
+
+    public int put(String var, int val) { return val; }
+    public int get(int pos, String var) throws EvalException { return 0; }
+
+}
