ca021a67c0845b886b1b167f786a8d8498a310eb
Jason Egbert <jasonegbert@u.boisestate.edu>
2018-09-18 09:00:01 -0600

    committing modifications to the branch

--- LA1-2/super-duper.scm
+++ LA1-2/super-duper.scm
@@ -1,12 +1,34 @@
 #!/bin/guile -s
 !#
+;*******************************************************************************
+; Program: Super-Duper program
+; Function: takes atoms from source and iterates them count times
+; Class: CS354-002
+; Date: 09/17/2018
+; Author: Jason Egbert
+;*******************************************************************************
 
-; Scheme super-duper program 
+(define (replicate element count)
+	(if (list? element)
+		(super-duper element count)
+	
+		(
+  			(if (< count 2)
+				(cons element '())
+			)
+			(if (> count 1)
+				(cons element (replicate element (+ count -1)))
+			)
+		)
+	)
+)
 
 (define (super-duper source count)
-	(if (null? source)
-		0
-	((car source) (super-duper (cdr source) count))))
-;function should not produce any output
-(display (super-duper source count))
-(display "\n")
+	(if (null? (car source)) ; source is empty
+		(list '())
+
+		(list (replicate (car source) count) (super-duper (cdr source) count))
+	)
+)
+
+(display (replicate '1 2))
