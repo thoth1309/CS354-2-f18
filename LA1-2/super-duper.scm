568b74c126d1d2bb826006f81aaa81fff5f24a3b
Jason Egbert <jasonegbert@u.boisestate.edu>
2018-09-19 15:53:54 -0600

    first of the final experiments

--- LA1-2/super-duper.scm
+++ LA1-2/super-duper.scm
@@ -8,27 +8,60 @@
 ; Author: Jason Egbert
 ;*******************************************************************************
 
-(define (replicate element count)
-	(if (list? element)
-		(super-duper element count)
-	
-		(
-  			(if (< count 2)
-				(cons element '())
-			)
-			(if (> count 1)
-				(cons element (replicate element (+ count -1)))
-			)
+(define (copy element count)
+	(cond
+	  	((> count 1)
+			(cons (super-duper (car element) (+ count -1)) (super-duper (cdr element) (+ count -1)))
 		)
 	)
 )
 
-(define (super-duper source count)
-	(if (null? (car source)) ; source is empty
-		(list '())
-
-		(list (replicate (car source) count) (super-duper (cdr source) count))
+(define (super-duper list count)
+	(cond
+		((< count 2)
+			(cons list '())
+		)
+		((not (list? list))
+			(cons list '())
+		)
+		(else
+			(copy list count)
+		)
 	)
 )
 
-(display (replicate '1 2))
+(display "(super-duper 123 1) = ")
+(display (super-duper 123 1))
+(display "\n")
+
+(display "(super-duper 123 2) = ")
+(display (super-duper 123 2))
+(display "\n")
+
+(display "(super-duper '() 1) = ")
+(display (super-duper '() 1))
+(display "\n")
+
+(display "(super-duper '() 2) = ")
+(display (super-duper '() 2))
+(display "\n")
+
+(display "(super-duper '(x) 1) = ")
+(display (super-duper '(x) 1))
+(display "\n")
+
+(display "(super-duper '(x) 2) = ")
+(display (super-duper '(x) 2))
+(display "\n")
+
+(display "(super-duper '(x y) 1) = ")
+(display (super-duper '(x y) 1))
+(display "\n")
+
+(display "(super-duper '(x y) 2) = ")
+(display (super-duper '(x y) 2))
+(display "\n")
+
+(display "(super-duper '((a b) y) 3) = ")
+(display (super-duper '((a b) y) 3))
+(display "\n")
