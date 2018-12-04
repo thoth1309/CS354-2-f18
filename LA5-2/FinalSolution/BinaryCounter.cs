7fd795c90f63817006631b254a130fbf2a9e250b
Jason Egbert <jasonegbert@u.boisestate.edu>
2018-12-03 19:46:44 -0700

    Added wrong file, fixing it

---
+++ LA5-2/FinalSolution/BinaryCounter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows.Forms;
+
+public class BinaryCounter : Label
+{
+
+    private int count;
+
+    public BinaryCounter(int count) : base()
+    {
+        Location = new System.Drawing.Point(255, 25);
+        Name = "BinaryCounter";
+        Text = Convert.ToString(count,2);
+        TabIndex = 1;
+        this.count = count;
+    }
+
+    public virtual void actionPerformed(object sender, System.EventArgs e)
+    {
+        Text = Convert.ToString(++count,2);
+    }
+
+}
