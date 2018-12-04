136558ba7e96ef6d96de06f37fcaa16c97384e39
Jason Egbert <jasonegbert@u.boisestate.edu>
2018-12-03 19:40:22 -0700

    Actual solution

---
+++ LA5-2/FinalSolution/LightSwitch.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows.Forms;
+
+public class LightSwitch : Form
+{
+
+    private ToggleButton button;
+    private BinaryCounter counter;
+
+    public LightSwitch()
+    {
+        Name = "LightSwitch";
+        Text = "LightSwitch";
+        SuspendLayout();
+        button = new ToggleButton("off", "on");
+        counter = new BinaryCounter(0);
+        button.Click += new System.EventHandler(counter.actionPerformed);
+
+
+        ClientSize = new System.Drawing.Size(500, 200);
+        Controls.AddRange(new System.Windows.Forms.Control[]
+        {
+            button, counter
+        });
+
+        ResumeLayout(false);
+    }
+
+    public static void Main(string[] args)
+    {
+        Application.Run(new LightSwitch());
+    }
+
+}
---
+++ LA5-2/FinalSolution/ToggleButton.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows.Forms;
+
+public class ToggleButton : Button
+{
+
+    private string label1, label2;
+
+    public ToggleButton(string label1, string label2) : base()
+    {
+        Location = new System.Drawing.Point(175, 20);
+        Text = label1;
+        TabIndex = 0;
+        this.label1 = label1;
+        this.label2 = label2;
+        Click += new System.EventHandler(this.actionPerformed);
+    }
+
+    private void actionPerformed(object sender, System.EventArgs e)
+    {
+        string s = label1;
+        label1 = label2;
+        label2 = s;
+        Text = label1;
+    }
+
+}
